c0335178746b99f22175bf58a700b3e7bfe8ef25
Ganesh Srambikal <ganesh.srambikal@gmail.com>
2024-06-20 23:17:00 +0530

    addes meta data and fixes banner css

--- client/app/components/Banner.tsx
+++ client/app/components/Banner.tsx
@@ -1,10 +1,20 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-
+import Github from '../../assets/github-logo.png'
 function Banner() {
     return (
-        <div className='mt-5'>
-            <span className='font-mono'>Made With <span className='text-red-500'>&#10084;</span> by <Link href='https://github.com/GaneshSrambikal'>@GaneshSrambikal</Link></span>
+        <div className='mt-5 text-center flex justify-center items-center'>
+            <span className='font-mono'>Made With
+                <span className='text-red-500 text-xl mr-1 ml-1 relative top-1'> &#10084;</span> by
+                <Image
+                    src={Github}
+                    alt='Github Logo'
+                    width={15}
+                    height={5}
+                    className='inline mr-2 ml-2'
+                />
+                <Link href='https://github.com/GaneshSrambikal'>@GaneshSrambikal</Link></span>
         </div>
     )
 }
