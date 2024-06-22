b8deee6dc7f0632e1edfe9e44569c6fbaba8022a
Ganesh Srambikal <ganesh.srambikal@gmail.com>
2024-06-22 14:00:23 +0530

    addes button state

--- client/app/Product/Cards/ProductCard/page.tsx
+++ client/app/Product/Cards/ProductCard/page.tsx
@@ -1,15 +1,19 @@
 'use client'
 //@ts-nocheck
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useToast } from '@/components/ui/use-toast'
-
+import { Loader2 } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 type MouseEvent = React.MouseEvent<HTMLButtonElement>
 const ProductCard = () => {
     const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://razorpay-integration-nextjs-server.vercel.app'
     const [amount, setAmount] = useState(259900)
+    const [loading, setLoading] = useState(false)
     const { toast } = useToast()
     const handlePayment = async (e: MouseEvent) => {
         e.preventDefault();
+        setLoading(true)
+        const clsBtn = document.getElementsByClassName('razorpay-container')
         const currency: string = 'INR';
         const receiptId: string = '123456789';
         const orders = await fetch(`${BASE_URL}/orders`, {
@@ -25,6 +29,13 @@ const ProductCard = () => {
         })
         const response = await orders.json()
         console.log('Payment Authorized')
+       
+        //@ts-ignore
+        if(clsBtn[0].style['display'] === 'block'){
+            setLoading(true)
+        }else{
+            setLoading(false)
+        }
         const option: object = {
             key: '',
             amount,
@@ -44,13 +55,11 @@ const ProductCard = () => {
                     },
                     body: JSON.stringify(body)
                 })
-                const jsonRes = await validate.json()
-                console.log('Order Paid');
+                // const jsonRes = await validate.json()
                 toast({
                     title: 'Order Placed Successfully!.',
                     description: `You have purchased FLIGHT Jacket Green for Rs.${new Intl.NumberFormat().format(Number(amount / 100))} on ${new Date().toDateString()}`
                 })
-
 
             },
             prefill: {
@@ -102,11 +111,23 @@ const ProductCard = () => {
                                 <span className="text-xl font-extralight line-through text-gray-400">Rs.5999 /-</span>
                                 <span className="text-3xl font-bold">{`Rs.${new Intl.NumberFormat().format(amount / 100)}/-`}</span>
                             </div>
-                            <button
-                                onClick={handlePayment}
-                                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-black text-white hover:bg-primary/90 h-10 px-4 py-2">
-                                Buy Now
-                            </button>
+                            {loading ?
+
+                                <Button
+                                    disabled
+                                    onClick={handlePayment}
+                                    className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-black text-white hover:bg-primary/90 h-10 px-4 py-2">
+                                    <Loader2 className='animate-spin' display={loading ? 'block' : 'hidden'} />  Buy Now
+                                </Button>
+                                :
+                                <Button
+
+                                    onClick={handlePayment}
+                                    className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-black text-white hover:bg-primary/90 h-10 px-4 py-2">
+                                    Buy Now
+                                </Button>
+                            }
+
                         </div>
                     </div>
                 </div>
